2ae8b5bec667d66b8be6168f558b5046625f63f6
Megopixapb <90756213+Megopixapb@users.noreply.github.com>
2024-10-10 21:13:37 +0300

    Updating logins file

--- logins.lua
+++ logins.lua
@@ -20,16 +20,6 @@ return array (
     'model' => 'no',
     'ip' => 1,
   ),
-  555 => 
-  array (
-    'password' => 'first_run',
-    'expiry' => '20241030',
-    'uid' => '1',
-    'getUID' => 1,
-    'brand' => 'no',
-    'model' => 'no',
-    'ip' => 1,
-  ),
   7112767513 => 
   array (
     'password' => 'updated_password',
