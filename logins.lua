239c3abeb321060fb654df00b7cb71ddd28d1141
Megopixapb <90756213+Megopixapb@users.noreply.github.com>
2024-10-10 13:28:38 +0300

    Updating logins file

---
+++ logins.lua
@@ -0,0 +1,43 @@
+<?php
+return array (
+  6423477344 => 
+  array (
+    'password' => 'first_run',
+    'expiry' => '20241030',
+    'uid' => '1',
+    'getUID' => 1,
+    'brand' => 'no',
+    'model' => 'no',
+    'ip' => 1,
+  ),
+  6699698602 => 
+  array (
+    'password' => 'first_run',
+    'expiry' => '20241030',
+    'uid' => '1',
+    'getUID' => 1,
+    'brand' => 'no',
+    'model' => 'no',
+    'ip' => 1,
+  ),
+  7112767513 => 
+  array (
+    'password' => 'updated_password',
+    'expiry' => '20241030',
+    'uid' => '10003',
+    'getUID' => 'd1821a632a98402099581238d6ea91d9',
+    'brand' => 'Xiaomi',
+    'model' => '22081212UG',
+    'ip' => 1,
+  ),
+  555 => 
+  array (
+    'password' => 'first_run',
+    'expiry' => '20241030',
+    'uid' => '1',
+    'getUID' => 1,
+    'brand' => 'no',
+    'model' => 'no',
+    'ip' => 1,
+  ),
+);
