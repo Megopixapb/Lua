3237977a9c64df7ea4d4a8b3043ed112f06636ef
Megopixapb <90756213+Megopixapb@users.noreply.github.com>
2024-10-13 21:55:38 +0300

    Updating logins file

--- logins.lua
+++ logins.lua
@@ -22,12 +22,12 @@ return array (
   ),
   7112767513 => 
   array (
-    'password' => 'first_run',
+    'password' => 'updated_password',
     'expiry' => '20241030',
-    'uid' => '1',
-    'getUID' => 'no',
-    'brand' => 'no',
-    'model' => 'no',
-    'ip' => 'no',
+    'uid' => '10003',
+    'getUID' => '3bbaaf8f676247fcad306e07eecab7dd',
+    'brand' => 'Xiaomi',
+    'model' => '22081212UG',
+    'ip' => '1d4c3cdf9a3add53',
   ),
 );
