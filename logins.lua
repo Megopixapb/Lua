eb8a1c0d3b5f8a6c0fcdced70f93a0dbcb80e235
Megopixapb <90756213+Megopixapb@users.noreply.github.com>
2024-10-17 16:29:33 +0300

    Updating logins file

--- logins.lua
+++ logins.lua
@@ -13,7 +13,7 @@ return array (
   7112767513 => 
   array (
     'password' => 'updated_password',
-    'expiry' => '20241017',
+    'expiry' => '20241016',
     'uid' => '10003',
     'getUID' => 'e6a38f008d8c473886e51dd3511db933',
     'brand' => 'Xiaomi',
